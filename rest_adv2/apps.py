bd1ff77741969cba2f0ec59d8488a0d78d81d99e
Cohen <work_id_tjr@163.com>
2020-03-22 12:43:55 +0800

    delete pages

--- rest_adv2/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RestAdvConfig(AppConfig):
-    name = 'rest_adv'
--- rest_adv2/templatetags/rest_adv_template_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from rest_adv.models import Review,UserProfile
-
-register = template.Library()
-
-@register.inclusion_tag('rest_adv/show_review.html')
-def show_review(rev):
-    return {'review': rev,
-            'user':rev.user}
